8c1a9d817b07c541cb3c76ca05660828b3f90ff9
Mithilesh <mithileshchaurasiya1627@gmail.com>
2025-04-07 19:21:39 +0530

    move web.py file to PIMA Diabetes folder

--- PIMA Diabetes/web.py
+++ PIMA Diabetes/web.py
@@ -1,12 +1,7 @@
 import pickle
 import numpy as np
 import streamlit as st
-
-<<<<<<< HEAD:web.py
-loader = pickle.load(open('PIMA Diabetes/training_model.sav', 'rb'))
-=======
 loader = pickle.load(open('training_model.sav', 'rb'))
->>>>>>> b2efe55... remove web.py file to PIMA Diabetes folder:PIMA Diabetes/web.py
 
 def diabetes_prediction(input_data):
     input_data_np_reshape=np.asarray(input_data).reshape(1,-1)
